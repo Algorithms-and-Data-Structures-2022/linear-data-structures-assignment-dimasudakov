31e8841eee25db1e383f1eec5167cc1fa01ff3c3
Dima Sudakov <sudakov.dima.2014@yandex.ru>
2022-04-02 00:14:01 +0300

    Затащил)

--- src/array_stack.cpp
+++ src/array_stack.cpp
@@ -3,82 +3,95 @@
 #include <algorithm>  // copy, fill
 #include <stdexcept>  // invalid_argument (НЕЛЬЗЯ ИСПОЛЬЗОВАТЬ)
 
+using namespace std;
+
 namespace assignment {
 
-  ArrayStack::ArrayStack(int capacity) {
-
-    // выбрасываем ошибку, если указана неположительная емкость стека
-    if (capacity <= 0) {
-      throw std::invalid_argument("capacity is not positive");
+    ArrayStack::ArrayStack(int capacity) {
+        if (capacity <= 0) {
+            throw invalid_argument("capacity is not positive");
+        }
+        data_ = new int[capacity];
+        fill(data_, data_ + capacity, 0);
+        capacity_ = capacity;
     }
 
-    // Write your code here ...
-  }
-
-  ArrayStack::~ArrayStack() {
-    // Write your code here ...
-  }
-
-  void ArrayStack::Push(int value) {
-    // Write your code here ...
-  }
-
-  bool ArrayStack::Pop() {
-    // Write your code here ...
-    return false;
-  }
-
-  void ArrayStack::Clear() {
-    // Write your code here ...
-  }
-
-  std::optional<int> ArrayStack::Peek() const {
-    // Write your code here ...
-    return std::nullopt;
-  }
-
-  bool ArrayStack::IsEmpty() const {
-    // Write your code here ...
-    return false;
-  }
-
-  int ArrayStack::size() const {
-    // Write your code here ...
-    return 0;
-  }
-
-  int ArrayStack::capacity() const {
-    // Write your code here ...
-    return 0;
-  }
-
-  bool ArrayStack::Resize(int new_capacity) {
-    // Write your code here ...
-    return false;
-  }
-
-  // ДЛЯ ТЕСТИРОВАНИЯ
-  ArrayStack::ArrayStack(const std::vector<int>& values, int capacity) {
-
-    size_ = static_cast<int>(values.size());
-    capacity_ = capacity;
-
-    data_ = new int[capacity]{};
-
-    std::copy(values.data(), values.data() + size_, data_);
-  }
-
-  std::vector<int> ArrayStack::toVector(std::optional<int> size) const {
-
-    if (capacity_ == 0 || data_ == nullptr) {
-      return {};
+    ArrayStack::~ArrayStack() {
+        size_ = capacity_ = 0;
+        data_ = nullptr;
     }
 
-    if (size.has_value()) {
-      return {data_, data_ + size.value()};
+    void ArrayStack::Push(int value) {
+        if(size_ == capacity_) {
+            capacity_ += kCapacityGrowthCoefficient;
+            int *lol = new int[capacity_];
+            fill(lol, lol + capacity_, 0);
+            for(int i = 0; i < size_; i++) lol[i] = data_[i];
+            data_ = lol;
+        }
+        data_[size_++] = value;
     }
 
-    return {data_, data_ + capacity_};
-  }
+    bool ArrayStack::Pop() {
+        if(size_ == 0) return false;
+        size_--;
+        data_[size_] = 0;
+        return true;
+    }
+
+    void ArrayStack::Clear() {
+        size_ = 0;
+        fill(data_, data_ + capacity_, 0);
+    }
+
+    std::optional<int> ArrayStack::Peek() const {
+        if(size_ == 0) return std::nullopt;
+        return data_[size_ - 1];
+    }
+
+    bool ArrayStack::IsEmpty() const {
+        return (size_ == 0);
+    }
+
+    int ArrayStack::size() const {
+        return size_;
+    }
+
+    int ArrayStack::capacity() const {
+        return capacity_;
+    }
+
+    bool ArrayStack::Resize(int new_capacity) {
+        if(new_capacity <= capacity_) return false;
+        int *lol = new int[new_capacity];
+        copy(data_, data_ + capacity_, lol);
+        capacity_ = new_capacity;
+        data_ = lol;
+        return true;
+    }
+
+    // ДЛЯ ТЕСТИРОВАНИЯ
+    ArrayStack::ArrayStack(const std::vector<int> &values, int capacity) {
+
+        size_ = static_cast<int>(values.size());
+        capacity_ = capacity;
+
+        data_ = new int[capacity]{};
+
+        std::copy(values.data(), values.data() + size_, data_);
+    }
+
+    std::vector<int> ArrayStack::toVector(std::optional<int> size) const {
+
+        if (capacity_ == 0 || data_ == nullptr) {
+            return {};
+        }
+
+        if (size.has_value()) {
+            return {data_, data_ + size.value()};
+        }
+
+        return {data_, data_ + capacity_};
+    }
 
 }  // namespace assignment
--- src/linked_list.cpp
+++ src/linked_list.cpp
@@ -1,111 +1,199 @@
 #include "assignment/linked_list.hpp"
 
+using namespace std;
+
 namespace assignment {
 
-  LinkedList::~LinkedList() {
+    LinkedList::~LinkedList() {
 
-    // эквивалентно очистке списка
-    LinkedList::Clear();
-  }
-
-  void LinkedList::Add(int value) {
-    // Write your code here ...
-  }
-
-  bool LinkedList::Insert(int index, int value) {
-    // Write your code here ...
-    return false;
-  }
-
-  bool LinkedList::Set(int index, int new_value) {
-    return false;
-  }
-
-  std::optional<int> LinkedList::Remove(int index) {
-    // Write your code here ...
-    return std::nullopt;
-  }
-
-  void LinkedList::Clear() {
-    // Write your code here ...
-  }
-
-  std::optional<int> LinkedList::Get(int index) const {
-    // Write your code here ...
-    return std::nullopt;
-  }
-
-  std::optional<int> LinkedList::IndexOf(int value) const {
-    // Write your code here ...
-    return std::nullopt;
-  }
-
-  bool LinkedList::Contains(int value) const {
-    return false;
-  }
-
-  bool LinkedList::IsEmpty() const {
-    return false;
-  }
-
-  int LinkedList::size() const {
-    return 0;
-  }
-
-  std::optional<int> LinkedList::front() const {
-    // Write your code here ...
-    return std::nullopt;
-  }
-
-  std::optional<int> LinkedList::back() const {
-    // Write your code here ...
-    return std::nullopt;
-  }
-
-  Node* LinkedList::FindNode(int index) const {
-    // Write your code here ...
-    return nullptr;
-  }
-
-  // ДЛЯ ТЕСТИРОВАНИЯ
-  LinkedList::LinkedList(const std::vector<int>& values) {
-
-    if (values.empty()) {
-      return;
+        // эквивалентно очистке списка
+        LinkedList::Clear();
     }
 
-    auto* curr_node = new Node(values.front());
-    front_ = curr_node;
-
-    for (int index = 1; index < values.size() - 1; ++index) {
-      curr_node->next = new Node(values[index]);
-      curr_node = curr_node->next;
+    void LinkedList::Add(int value) {
+        if(size_ == 0) {
+            front_ = new Node(value, nullptr);
+            back_ = front_;
+            size_++;
+            return;
+        }
+        Node *p = new Node(value);
+        back_ -> next = p;
+        back_ = p;
+        size_++;
     }
 
-    if (values.size() == 1) {
-      back_ = front_;
-    } else {
-      curr_node->next = new Node(values.back());
-      back_ = curr_node->next;
+    bool LinkedList::Insert(int index, int value) {
+        if(index < 0 || size_ < index) return false;
+        if(index == size_) {
+            Add(value);
+            return true;
+        }
+        if(index == 0) {
+            Node *p = new Node(value);
+            p -> next = front_;
+            front_ = p;
+            size_++;
+            return true;
+        }
+        Node *p = front_;
+        for(int i = 0; i < index - 1; i++) {
+            p = p -> next;
+        }
+        Node *lol = new Node(value);
+        lol -> next = p -> next;
+        p -> next = lol;
+        size_++;
+        return true;
     }
 
-    size_ = static_cast<int>(values.size());
-  }
-
-  std::vector<int> LinkedList::toVector() const {
-
-    if (front_ == nullptr || size_ == 0) {
-      return {};
+    bool LinkedList::Set(int index, int new_value) {
+        if(index < 0 || size_ <= index) return false;
+        Node *cur = front_;
+        for(int i = 0; i < index; i++) {
+            cur = cur -> next;
+        }
+        cur -> value = new_value;
+        return true;
     }
 
-    std::vector<int> array;
-    array.reserve(size_);
+    std::optional<int> LinkedList::Remove(int index) {
+        if(index < 0 || size_ <= index) return nullopt;
+        if(size_ == 1) {
+            int res = front_ -> value;
+            Clear();
+            return res;
+        }
+        if(index == 0) {
+            size_--;
+            int res = front_ -> value;
+            Node *p = front_;
+            front_ = front_ -> next;
+            delete p;
+            return res;
+        }
 
-    for (auto* node = front_; node != nullptr; node = node->next) {
-      array.push_back(node->value);
+        Node *p = front_;
+        for(int i = 0; i < index - 1; i++) p = p -> next;
+
+        int res = (p -> next) -> value;
+        if(index == size_ - 1) {
+            p -> next = nullptr;
+            back_ = p;
+            size_--;
+            return res;
+        }
+        p -> next = (p -> next) -> next;
+        size_--;
+        return res;
     }
 
-    return array;
-  }
+    void LinkedList::Clear() {
+        Node *cur = front_;
+        for(int i = 0; i < size_; i++) {
+            if(i == size_ - 1) {
+                delete cur;
+                break;
+            }
+            Node* mem = cur -> next;
+            delete cur;
+            cur = mem;
+        }
+        back_ = front_ = nullptr;
+        size_ = 0;
+    }
+
+    std::optional<int> LinkedList::Get(int index) const {
+        if(index < 0 || size_ <= index) return nullopt;
+        Node cur = *front_;
+        for(int i = 0; i < index; i++) cur = *cur.next;
+        return cur.value;
+    }
+
+    std::optional<int> LinkedList::IndexOf(int value) const {
+        Node *cur = front_;
+        for(int i = 0; i < size_; i++) {
+            if(cur -> value == value) return i;
+            cur = cur -> next;
+        }
+        return nullopt;
+    }
+
+    bool LinkedList::Contains(int value) const {
+        Node *cur = front_;
+        for(int i = 0; i < size_; i++) {
+            if(cur -> value == value) return true;
+            cur = cur -> next;
+        }
+        return false;
+    }
+
+    bool LinkedList::IsEmpty() const {
+        return (size_ == 0);
+    }
+
+    int LinkedList::size() const {
+        return size_;
+    }
+
+    std::optional<int> LinkedList::front() const {
+        if(size_ == 0) return nullopt;
+        return front_ -> value;
+    }
+
+    std::optional<int> LinkedList::back() const {
+        if(size_ == 0) return nullopt;
+        return back_ -> value;
+    }
+
+    Node *LinkedList::FindNode(int index) const {
+        if(index < 0 || size_ <= index) return nullptr;
+        Node* cur = front_;
+        for(int i = 0; i < index; i++) {
+            cur = (*cur).next;
+        }
+        return cur;
+    }
+
+    // ДЛЯ ТЕСТИРОВАНИЯ
+    LinkedList::LinkedList(const std::vector<int> &values) {
+
+        if (values.empty()) {
+            return;
+        }
+
+        auto *curr_node = new Node(values.front());
+        front_ = curr_node;
+
+        for (int index = 1; index < values.size() - 1; ++index) {
+            curr_node->next = new Node(values[index]);
+            curr_node = curr_node->next;
+        }
+
+        if (values.size() == 1) {
+            back_ = front_;
+        } else {
+            curr_node->next = new Node(values.back());
+            back_ = curr_node->next;
+        }
+
+        size_ = static_cast<int>(values.size());
+    }
+
+    std::vector<int> LinkedList::toVector() const {
+
+        if (front_ == nullptr || size_ == 0) {
+            return {};
+        }
+
+        std::vector<int> array;
+        array.reserve(size_);
+
+        for (auto *node = front_; node != nullptr; node = node->next) {
+            array.push_back(node->value);
+        }
+
+        return array;
+    }
 
 }  // namespace assignment
